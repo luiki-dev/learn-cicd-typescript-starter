2cfd953d2cbd07562141acb2cf0dd00185b92112
Przemek Lewicki <przemyslaw.lewicki.dev@proton.me>
2026-09-03 22:43:39 +0200

    Add tests for getAPIKey

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,32 @@
+import { IncomingHttpHeaders } from "http";
+import { getAPIKey } from "../api/auth.js";
+import { describe, expect, test } from "vitest";
+
+
+describe("getAPIKey tests", () => {
+  test("getAPIKey basic flow", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey abc123",
+    };
+    expect(getAPIKey(headers)).toBe("abc123");
+  });
+
+  test("getAPIKey missing authorization header", () => {
+    const headers: IncomingHttpHeaders = {};
+    expect(getAPIKey(headers)).toBe(null);
+  });
+
+  test("getAPIKey malformed authorization header (missing key)", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey",
+    };
+    expect(getAPIKey(headers)).toBe(null);
+  });
+
+  test("getAPIKey wrong auth scheme", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "Bearer abc123",
+    };
+    expect(getAPIKey(headers)).toBe(null);
+  });
+});
